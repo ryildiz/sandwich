f802e3384219a6e9cfe35afda16e62796edd5616
Ramazan Yildiz <ramazangsu@gmail.com>
2017-08-08 22:09:44 +0300

    sandwich model
    Signed-off-by: Ramazan Yildiz <ramazangsu@gmail.com>

--- src/model/Sandwich.ts
+++ src/model/Sandwich.ts
@@ -1,11 +1,16 @@
 export class Sandwich {
 
+    name?:string;
+    icerik?:string;
     ekmektipi?: string;
     ekmekboyu?: string;
     peynir?: {
         beyaz?:string,
+        beyazdilimsayisi?:number,
         kasar?:string,
+        kasardilimsayisi?:number,
         dil?:string
+        dildilimsayisi?:number
     };
 
     yesillik?: {
@@ -25,14 +30,55 @@ export class Sandwich {
 
     fiyat?:number;
 
+
+    constructor() {
+        this.peynir.kasardilimsayisi=1;
+        this.peynir.beyazdilimsayisi=1;
+        this.peynir.dildilimsayisi=1;
+        this.icerik = this.getText();
+
+    }
+
+    dilArtir(){
+        if(this.peynir.dildilimsayisi<this.MAXDILIM)
+            this.peynir.dildilimsayisi++;
+    }
+
+    dilAzalt(){
+        if(this.peynir.dildilimsayisi>1)
+            this.peynir.dildilimsayisi--;
+    }
+
+    beyazArtir(){
+        if(this.peynir.beyazdilimsayisi<this.MAXDILIM)
+            this.peynir.beyazdilimsayisi++;
+    }
+
+    beyazAzalt(){
+        if(this.peynir.beyazdilimsayisi>1)
+            this.peynir.beyazdilimsayisi--;
+    }
+
+    kasarArtir(){
+        if(this.peynir.kasardilimsayisi<this.MAXDILIM)
+            this.peynir.kasardilimsayisi++;
+    }
+
+    kasarAzalt(){
+        if(this.peynir.kasardilimsayisi>1)
+            this.peynir.kasardilimsayisi--;
+    }
+
+    private MAXDILIM = 10;
     private SPACE = ' ';
 
     getText() {
 
+       let desc=
         (this.ekmekboyu === 'tam' ? 'Tam' : 'Yarým') +  this.SPACE +(this.ekmektipi === 'tahilli' ? 'tahýllý' : 'beyaz') + this.SPACE + 'ekmek,' + this.SPACE +
-        this.peynir ? (this.peynir.beyaz ? 'beyaz peynir,' + this.SPACE : '') : '' +
-        this.peynir ? (this.peynir.kasar ? 'kaþar peyniri,' + this.SPACE : '') : '' +
-        this.peynir ? (this.peynir.dil ? 'dil peyniri' + this.SPACE : '') : '' +
+        this.peynir ? (this.peynir.beyaz ? this.peynir.beyazdilimsayisi + this.SPACE + 'dilim beyaz peynir,'  + this.SPACE : '') : '' +
+        this.peynir ? (this.peynir.kasar ? this.peynir.kasardilimsayisi + this.SPACE + 'dilim kaþar peyniri,' + this.SPACE : '') : '' +
+        this.peynir ? (this.peynir.dil ? this.peynir.dildilimsayisi + this.SPACE + 'dilim dil peyniri' + this.SPACE : '') : '' +
 
         this.yesillik ? (this.yesillik.domates ? 'domates,' + this.SPACE : '') : '' +
         this.yesillik ? (this.yesillik.salatalik ? 'salatalýk,' + this.SPACE : '') : '' +
@@ -41,7 +87,9 @@ export class Sandwich {
         this.susleme ? (this.susleme.zeytinyagi ? 'zeytinyaðý,' + this.SPACE : '') : '' +
 
         this.sos ? (this.sos.zeytinezmesi ? 'zeytin ezmesi,' + this.SPACE : '') : '' +
-        this.sos ? (this.sos.acuka ? 'dil peyniri' + this.SPACE : '') : ''
+        this.sos ? (this.sos.acuka ? 'dil peyniri' + this.SPACE : '') : '';
+
+       return desc;
 
     }
 
